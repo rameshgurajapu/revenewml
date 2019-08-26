21f667ec56e47280d0e89d3bc209528697ae1fa2
AvianaGlobal <michaelj@avianaglobal.com>
2019-08-26 12:05:53 -0700

    Update scoring.py

--- src/scoring.py
+++ src/scoring.py
@@ -284,7 +284,7 @@ def main(database, dsn):
     table_name = 'ModelScores'
 
     # Write to SQL database
-    df_output.to_sql(name=table_name, con=engine, if_exists='append', schema=schema, index_label='ID')
+    df_output.to_sql(name=table_name, con=engine, if_exists='replace', schema=schema, index_label='ID')
 
     # Stop timer
     end = timer()
